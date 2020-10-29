f323c4e7df2f58d441017f49dbdd672f9b7fe52a
Van-Hoang Nguyen <vanguyen@paypal.com>
2020-10-29 21:40:58 +0800

    Finalize reproducibility

--- train.py
+++ train.py
@@ -21,12 +21,14 @@ from loader.augmentor import AddGaussianNoise
 import torch.optim as optim
 from utils import get_exp_name
 
-torch.manual_seed(0)
+SEED = 42
+
+torch.manual_seed(SEED)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
-np.random.seed(0)
+np.random.seed(SEED)
 
-TRIPLET_LOSS_WEIGHT = 0  # Tune this
+TRIPLET_LOSS_WEIGHT = 0.0  # Tune this
 
 
 def load_transform(image_size=256, crop_size=224):
@@ -52,8 +54,8 @@ def load_common_transform(image_size=256, crop_size=224):
     mean = [0.485, 0.456, 0.406]
     std = [0.229, 0.224, 0.225]
     train_transform = transforms.Compose([
-        transforms.RandomVerticalFlip(),
-        # transforms.RandomRotation(degrees=(-5, 5)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(degrees=(-15, 15)),
         transforms.Resize(image_size),
         transforms.CenterCrop(crop_size),
         transforms.ToTensor(),
@@ -62,8 +64,7 @@ def load_common_transform(image_size=256, crop_size=224):
     ])
 
     valid_transform = transforms.Compose([
-        transforms.Resize(image_size),
-        transforms.CenterCrop(crop_size),
+        transforms.Resize(size=(image_size, image_size), interpolation=2),
         transforms.ToTensor(),
         transforms.Normalize(mean=mean, std=std)
     ])
@@ -104,19 +105,17 @@ def load_small_cnn(num_classes, device):
 
 
 def load_wide_resnet(num_classes, device):
-    model = models.wide_resnet50_2(pretrained=True).to(device)
-    # trained_layer_indices = [7, 9]
-    trained_layer_indices = [6, 7, 9]
+    model = models.wide_resnet101_2(pretrained=True).to(device)
+    trained_layer_indices = [7, 9]
+    # trained_layer_indices = [6, 7, 9]
 
     for i, child in enumerate(model.children()):
-        print(i, child)
         if i not in trained_layer_indices:
             for param in child.parameters():
                 param.requires_grad = False
     num_ftrs = model.fc.in_features
-    # model.fc = nn.Sequential(nn.Linear(num_ftrs, 256), nn.Linear(256, num_classes)).to(device)
-    model.layer4 = nn.Sequential().to(device)
-    num_ftrs = 1024
+    # model.layer4 = nn.Sequential().to(device)
+    # num_ftrs = 1024
     model.fc = nn.Linear(num_ftrs, num_classes).to(device)
 
     train_transform, valid_transform = load_transform(image_size=256, crop_size=224)
@@ -171,9 +170,9 @@ def create_loaders(train_transform, valid_transform, fold_idx=0):
 def train_fn(model_name, fold_idx):
     print("Training {} on fold {}".format(model_name, fold_idx))
     num_classes = 3
-    epochs = 100
+    epochs = 30
 
-    device = torch.device("cuda:0")
+    device = torch.device("cuda:3")
     # device = torch.device("cpu")
 
     if model_name == "wide_resnet":
@@ -196,9 +195,10 @@ def train_fn(model_name, fold_idx):
                                                                               fold_idx=fold_idx)
     train_size = len(train_dataset)
 
-    optimizer = optim.Adam(params=filter(lambda p: p.requires_grad, model.parameters()), lr=0.0001, weight_decay=5e-3)
+    optimizer = optim.Adam(params=filter(lambda p: p.requires_grad, model.parameters()), lr=0.0001, weight_decay=1e-2)
     # optimizer = optim.Adam(params=filter(lambda p: p.requires_grad, model.parameters()), lr=0.001, weight_decay=0)
-    # lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.5)
+    # lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+    lr_scheduler = None
 
     best_acc_f1 = 0.
     triplet_criterion = TripletLoss(margin=1.0)   # Tune this
@@ -216,19 +216,20 @@ def train_fn(model_name, fold_idx):
                 pos_inputs.to(device), neg_inputs.to(device)
 
             anchor_output = model(inputs)
-            pos_output = model(pos_inputs)
-            neg_output = model(neg_inputs)
 
             supervised_loss = F.cross_entropy(anchor_output, labels)
-            triplet_loss = triplet_criterion(anchor_output, pos_output, neg_output)
             if TRIPLET_LOSS_WEIGHT == 0:
                 total_loss = supervised_loss
+                epoch_triplet_loss += 0
             else:
+                pos_output = model(pos_inputs)
+                neg_output = model(neg_inputs)
+                triplet_loss = triplet_criterion(anchor_output, pos_output, neg_output)
                 total_loss = (1 - TRIPLET_LOSS_WEIGHT) * supervised_loss + TRIPLET_LOSS_WEIGHT * triplet_loss
+                epoch_triplet_loss += triplet_loss.item()
 
             epoch_loss += total_loss.item()
             epoch_supervised_loss += supervised_loss.item()
-            epoch_triplet_loss += triplet_loss.item()
 
             # update the parameters
             optimizer.zero_grad()
@@ -239,8 +240,10 @@ def train_fn(model_name, fold_idx):
             train_predictions.extend(preds.detach().cpu().numpy())
             train_labels.extend(labels_cpu.numpy())
 
-        # lr_scheduler.step()
-        # train_lr = lr_scheduler.get_last_lr()[0]
+        if lr_scheduler is not None:
+            lr_scheduler.step()
+            train_lr = lr_scheduler.get_last_lr()[0]
+            writer.add_scalar(tag="train_lr", scalar_value=train_lr, global_step=epoch)
 
         avg_supervised_loss, avg_triplet_loss, avg_epoch_loss = epoch_supervised_loss / train_size, \
             epoch_triplet_loss / train_size, epoch_loss / train_size
@@ -256,7 +259,6 @@ def train_fn(model_name, fold_idx):
             else:
                 writer.add_scalar(tag="train_{}".format(metric_name), scalar_value=metric_values,
                                   global_step=epoch)
-        # writer.add_scalar(tag="train_lr", scalar_value=train_lr, global_step=epoch)
         train_losses = {
             "supervised_loss": avg_supervised_loss,
             "triplet_loss": avg_triplet_loss,
@@ -379,7 +381,7 @@ def eval_fn(model, valid_dataset, valid_loader, device, triplet_criterion, eval_
 def load_fold_models(fold, device):
     num_classes = 3
     wide_resnet, resnext, densenet = None, None, None
-    trained_model_dir = "trained_models_0.97945"
+    trained_model_dir = "trained_models"
     for folder in os.listdir(trained_model_dir):
         if folder.startswith("pretrained-wide-resnet-{}".format(fold)):
             wide_resnet_checkpoint_path = os.path.join(trained_model_dir, folder, "best.pth")
@@ -398,7 +400,7 @@ def load_fold_models(fold, device):
 
 if __name__ == "__main__":
     start_time = time.time()
-    for f in range(2, 3):
+    for f in range(0, 1):
         for model_n in ["wide_resnet"]:
             train_fn(model_n, f)
     end_time = time.time()
